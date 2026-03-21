c879794d3c8332ac13aed0fb7bf5262be4cd6eae
123046400-ops <123046400@upq.edu.mx>
2026-03-20 18:21:44 -0600

    practica_9

---
+++ miApi/app/data/db.py
@@ -0,0 +1,18 @@
+#para la conexion de ba se de datos 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+import os
+
+#1. defenimos URL de la conexión 
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://admin:123456@postgres:5432/DB_miapi")
+
+# 2. Crear el motor de la conexión
+
+engine= create_engine(DATABASE_URL)
+# 3. Creamos la gestión de sessiones
+SessionLocal= sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+
+    ,)
